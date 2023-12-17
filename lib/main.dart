ca9c4166bbaf30a525b614a5b9b4aac8fbe9fcbd
Surya J <suryaj.cse2022@citchennai.net>
2023-12-17 13:31:34 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'buttons.dart';
+
+void main() {
+  runApp(MyApp());
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
